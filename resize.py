32499cae61c2126fe7ff1ee169667a2231dfa5aa
Yuzuki Mimura <yuzuki725.m@gmail.com>
2019-05-06 20:55:31 +0900

    add size input

--- resize.py
+++ resize.py
@@ -3,9 +3,11 @@ import glob
 
 files = glob.glob("./img/*")
 
+w = int(input("width>>"))
+
 for fname in files:
     print(fname)
     img = cv2.imread(fname)
     height, width = img.shape[:2]
-    img2 = cv2.resize(img,(1200,int(height*(1200/width))))
+    img2 = cv2.resize(img,(w,int(height*(w/width))))
     cv2.imwrite("./r/r_" + fname[6:] , img2)
